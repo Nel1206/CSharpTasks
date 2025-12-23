750bfdc05f9b9f4cea00bde7f49eac742d4969ec
Ronel N.Nipa <ronelnisola@gmail.com>
2025-12-23 08:42:36 +0000

    Task4

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940c443dc45397566278509196ea6d3d0fb7d7ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+334d0a21687916d337a516f6345cf8b87d05df20")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
